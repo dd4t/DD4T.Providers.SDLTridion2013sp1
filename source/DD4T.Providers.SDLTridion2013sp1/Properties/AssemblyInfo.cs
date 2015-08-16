10b94edbdb2adaad1a5792a05897b24d649f1ae0
Thijs Borst <thijs.borst@incentro.com>
2015-08-16 12:35:43 +0200

    Little cleanup of Assembly info

--- source/DD4T.Providers.SDLTridion2013sp1/Properties/AssemblyInfo.cs
+++ source/DD4T.Providers.SDLTridion2013sp1/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DD4T.Providers.SDLTridion2013")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DD4T.Providers.SDLTridion2013sp1")]
+[assembly: AssemblyProduct("DD4T")]
+[assembly: AssemblyDescription("Dynamic Delivery for Tridion")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Amazon.com")]
-[assembly: AssemblyProduct("DD4T.Providers.SDLTridion2013")]
-[assembly: AssemblyCopyright("Copyright © Amazon.com 2013")]
+[assembly: AssemblyCompany("Open Source")]
+[assembly: AssemblyCopyright("Open Source Apache 2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5389e49-1c85-4623-b4a3-556b4311f7ae")]
+[assembly: Guid("2c95e09e-03b5-4a6d-8b19-6a46e38840ff")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha1")]
